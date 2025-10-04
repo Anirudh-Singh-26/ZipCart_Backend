b8cf9c82a50a0f54cc57460b4a58a1b689f26018
Anirudh-Singh-26 <anirudhrathore2003@gmail.com>
2025-10-04 19:52:00 +0530

    Updating files

--- index.js
+++ index.js
@@ -38,28 +38,16 @@ const corsOptions = {
   credentials: true, // allow cookies
 };
 
-// --- Middlewares ---
+// Middlewares
 app.use(cors(corsOptions));
 app.use(cookieParser());
 app.use(express.json());
 
-// --- Global Request Logger ---
-app.use((req, res, next) => {
-  const now = new Date().toISOString();
-  console.log(`[${now}] ${req.method} ${req.originalUrl} from ${req.ip}`);
-  next();
-});
-
-// --- Test route (backend wake-up) ---
+// Test route
 app.get("/ping", (req, res) => {
-  console.log(
-    `[PING] /ping requested at ${new Date().toISOString()} from ${req.ip}`
-  );
   res.json({ message: "pong" });
 });
 
-// Static files
-app.use("/images", express.static("uploads"));
 
 // API routes
 app.use("/api/user", userRoutes);
